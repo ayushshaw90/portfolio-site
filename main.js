e033ebbc1b7d437aa86b2f296748be5ad75a28c0
Ayush Shaw <ayushshaw127@gmail.com>
2022-06-02 19:47:10 +0530

    referenced all necessary elements for implementing intersection observer

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ console.log(navHeight)
 window.addEventListener("hashchange",()=>{
     window.scrollTo(window.scrollX, window.scrollY-navHeight)
 })
+
+let DivToMenu = new Map();
+let Home = document.getElementById("Home");
+let Contact = document.getElementById("Contact");
+let About = document.getElementById("About");
+let Skills = document.getElementById("Skills");
+let Projects = document.getElementById("Projects");
+
+let home_link = document.getElementById("home-link");
+let contact_link = document.getElementById("contact-link");
+let about_link = document.getElementById("about-link");
+let skills_link = document.getElementById("skills-link");
+let projects_link = document.getElementById("projects-link");
+DivToMenu.set(Home, home_link);
+DivToMenu.set(Contact, contact_link);
+DivToMenu.set(About, about_link);
+DivToMenu.set(Skills, skills_link);
+DivToMenu.set(Projects, projects_link);
+let observer= new IntersectionObserver(entries=>{
+    
+})
